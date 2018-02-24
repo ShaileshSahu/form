057c4190b821c74dd23f920130255587e3359be1
shailesh sahu <sahushailesh9@gmail.com>
2018-02-24 15:12:15 -0800

    started

---
+++ config/config.js
@@ -0,0 +1,2 @@
+
+module.exports = {Key:"mongodb://Shailesh:sahu1234@ds249128.mlab.com:49128/form"}
---
+++ index.js
@@ -0,0 +1,36 @@
+
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const config = require('./config/config');
+const url = require('url');
+
+mongoose.connect(config.Key);
+const schema = mongoose.Schema;
+var myschema = new schema({
+
+name :String,
+phone :Number,
+address:String,
+speacility:String,
+timing:String,
+services:String,
+areahead:String
+
+});
+const app =express();
+ const doctorDatabase=  mongoose.model("doctorDatabase",myschema);
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static(__dirname + '../public'));
+app.get('/',(req,res)=>{res.render('apps')});
+app.get('/database*',(req,res)=>{
+ var query =url.parse(req.url,true).query;
+
+ const data = new doctorDatabase({name:query.doctor,phone:query.phone,address:query.address,speacility:query.speacility,timing:query.timing,services:query.services,areahead:query.areahead });
+ data.save().then(()=>{console.log("Ok");});
+res.send(query.doctor);
+
+
+});
+app.listen('5000');
